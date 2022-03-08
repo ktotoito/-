dcbf0e1fe31217a6d8d5965ba25875e375668534
Ktotoito <90417163+ktotoito@users.noreply.github.com>
2022-03-08 17:42:04 +0400

    Add files via upload

---
+++ 3.Обработчик формы авторизации.py
@@ -0,0 +1,11 @@
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Wrong login or password", form=form)
+    return render_template('login.html', title='Authorization', form=form)
